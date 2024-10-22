9239291eb7dc3953c5d785218d93d33e05ec64ad
gst <pavhanys@gmail.com>
2024-10-22 12:02:46 +0200

    feat: add Payments page

---
+++ app/payments/page.tsx
@@ -0,0 +1,5 @@
+export default function Payments() {
+  return (
+    <h1 className="text-4xl font-bold">Payments</h1>
+  );
+}
--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps extends React.ComponentProps<"div"> {
 
 const Container = ({children, className, ...props}: ContainerProps) => {
   return (
-    <div {...props} className={cn("max-w-5xl mx-auto px-8", className)}>
+    <div {...props} className={cn("max-w-5xl mx-auto px-8 py-12", className)}>
       {children}
    </div>
   );
