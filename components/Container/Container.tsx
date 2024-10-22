6df30af7dadcaa8119e661c04a60b37344d81d46
gst <pavhanys@gmail.com>
2024-10-22 11:37:20 +0200

    feat: add Container component

---
+++ components/Container/Container.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@lib/utils";
+
+interface ContainerProps extends React.ComponentProps<"div"> {
+  children: React.ReactNode;
+}
+
+const Container = ({children, className, ...props}: ContainerProps) => {
+  return (
+    <div {...props} className={cn("max-w-5xl mx-auto px-8", className)}>
+      {children}
+   </div>
+  );
+}
+
+export default Container;
