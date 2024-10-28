7d96e2b373a1ed2a6cf3c67677c592e7f3c88e2b
gst <pavhanys@gmail.com>
2024-10-28 16:32:00 +0100

    fix: set list of variables as const

--- data/constants.ts
+++ data/constants.ts
@@ -2,14 +2,14 @@ export const PAYMENT_STATUSES = {
   'PENDING': 'Pending',
   'SUCCESS': 'Success',
   'CANCELED': 'Canceled',
-}
+} as const;
 
 export const PAYMENT_TYPES =   {
   'BANK_TRANSFER': 'Bank Transfer', 
   'APPLE_PAY': 'Apply Pay', 
   'GOOGLE_PAY': 'Google Pay',
   'CARD_ONLINE': 'Card Online',
-};
+} as const;
 
 export const CURRENCY = {
   'USD': 'en-US',
